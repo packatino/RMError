5c4d03500de1b5e900a98530c8ae1e5c2f39c738
Robert Mißbach <robert.missbach@holidaycheck.com>
2015-08-06 20:30:44 +0200

    created Xcode project

---
+++ RMError/RMError/RMError.m
@@ -0,0 +1,13 @@
+//
+//  RMError.m
+//  RMError
+//
+//  Created by Robert Mißbach on 06.08.15.
+//  Copyright (c) 2015 Robert Mißbach. All rights reserved.
+//
+
+#import "RMError.h"
+
+@implementation RMError
+
+@end
